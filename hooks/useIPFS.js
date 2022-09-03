7f73aa55bacc061e750f37cf47d3affc4eb5e323
VansRouges <evansagina57@gmail.com>
2022-09-03 01:21:33 +0100

    initial commit

--- hooks/useIPFS.js
+++ hooks/useIPFS.js
@@ -1,5 +1,19 @@
+// const useIPFS = (hash, filename) => {
+// return `https://gateway.pinata.cloud/ipfs/${hash}?filename=${filename}`
+// };
+
+// export default useIPFS;
+
+import { useState, useEffect } from 'react';
+
 const useIPFS = (hash, filename) => {
-return `https://gateway.ipfscdn.io/ipfs/${hash}?filename=${filename}`
+  const [file, setFile] = useState(null);
+
+  useEffect(() => {
+    setFile(`https://gateway.ipfscdn.io/ipfs/${hash}?filename=${filename}`);
+  }, []);
+
+  return file;
 };
 
 export default useIPFS;
